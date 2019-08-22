c302811278435e112943772df4468b1da7da46c8
alHasandev <alhasan99dev@gmail.com>
2019-08-22 15:47:20 +0800

    feature adding: add image to indexedDB

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-// prepare DOM Element
+// prepare DOM Element References
 const listsActive = document.getElementById('lists-active');
+const listsNonActive = document.getElementById('lists-nonactive');
 const formAddItem = document.getElementById('form-add-item');
 const inputItemName = document.getElementById('input-item-name');
+
+// prepare dummy data
+let listItems = [{
+    text: 'Item 1',
+    checked: false,
+    id: 1111111,
+  },
+  {
+    text: 'Item 2',
+    checked: false,
+    id: 1112222,
+  },
+  {
+    text: 'Item 3',
+    checked: true,
+    id: 1112223,
+  },
+  {
+    text: 'Item 4',
+    checked: true,
+    id: 1112244,
+  }
+];
+
+// function
+// push new item to list item array and render list item
 const addItem = function (text) {
   const item = {
     text,
@@ -13,27 +40,21 @@ const addItem = function (text) {
   listsActive.innerHTML = renderLists(listItems);
 }
 
-function renderLists(lists, checked = false) {
-  let html = '';
-  lists.forEach(item => {
-    if (item.checked === checked) html += listItem(item);
-  });
-
-  return html;
-
-}
-
+// return dom string of new item
 const listItem = function (item) {
-  let html = `<li class="list-item ${item.checked ? 'checked' : null}" data-key="${item.id}">`;
-  // html += `<label for="${item.id}" class="checklist circle">`;
-  html += `<input type="checkbox" class="checklist circle" ${item.checked ? 'checked' : null}>`;
-  // html += '</label>';
-  html += `<span>${item.text}</span>`;
+  let html = `<li class="list-item" data-key="${item.id}">`;
+  html += `<div class="checklist">`;
+  html += `<input type="checkbox" ${item.checked ? 'checked' : null}>`;
+  html += '<span class="checkmark"></span>';
+  html += `<label>${item.text}</label>`;
+  html += '</div>';
+  html += '<span class="symbol-right hover-danger">&#10006</span>';
   html += '</li>';
 
   return html;
 }
 
+// set item checked property when checklist clicked
 const toggleDone = function (key) {
   // find index
   const index = listItems.findIndex(item => item.id === Number(key));
@@ -54,42 +75,121 @@ const toggleDone = function (key) {
     resolve(listItems);
   });
   // }, 1000);
+}
+
+// function / delete list item
+const deleteItem = function (key) {
+  // find index
+  const index = listItems.findIndex(item => item.id === Number(key));
+
+  // remove array item by their index
+  if (index > -1) {
+    listItems.splice(index, 1);
+  }
 
 }
 
-let listItems = [];
-
-// listen to event
-// -- submit: form add item
-formAddItem.addEventListener('submit', (event) => {
-  // prevent default behaviour
-  event.preventDefault();
-
-  // get inputed text
-  let text = inputItemName.value.trim();
-
-  // check if text is empty
-  if (text != '') {
-    // add new item to active lists
-    addItem(text);
-  }
-
-  // reset & set focus to input
-  inputItemName.value = '';
-  inputItemName.focus();
-});
-
-// -- click: list item
-listsActive.addEventListener('click', (event) => {
-  if (event.target.classList.contains('checklist')) {
-    let itemKey = event.target.parentElement.dataset.key;
-
+// make action when checklist clicked
+const clickCheck = function (event) {
+  if (event.target.parentElement.classList.contains('checklist')) {
+    let itemKey = event.target.parentElement.parentElement.dataset.key;
+    // console.log(itemKey);
     toggleDone(itemKey).then(result => {
-      console.log(result);
+      // console.log(result);
 
       listsActive.innerHTML = renderLists(result);
+      listsNonActive.innerHTML = renderLists(result, true);
     });
   }
+}
+
+// make action when cross symbol clicked
+const clickCross = function (event) {
+  if (event.target.classList.contains('hover-danger')) {
+    if (confirm('Are you sure ?')) {
+      let itemKey = event.target.parentElement.dataset.key;
+
+      // delete item from lists
+      deleteItem(itemKey);
+
+      // re-render list items
+      listsActive.innerHTML = renderLists(listItems);
+      listsNonActive.innerHTML = renderLists(listItems, true);
+    }
+  }
+}
+
+// listen to event
+
+// check if component reference exist
+checkReference(listsNonActive, () => {
+  // -- submit: form add item
+  formAddItem.addEventListener('submit', (event) => {
+    // prevent default behaviour
+    event.preventDefault();
+
+    // get inputed text
+    let text = inputItemName.value.trim();
+
+    // check if text is empty
+    if (text != '') {
+      // add new item to active lists
+      addItem(text);
+    }
+
+    // reset & set focus to input
+    inputItemName.value = '';
+    inputItemName.focus();
+  });
 });
 
-// make promise
+// check if component reference exist
+checkReference(listsNonActive, () => {
+  // -- click: Active list item
+  listsActive.addEventListener('click', (event) => {
+    // call function when checklist clicked
+    clickCheck(event);
+    clickCross(event);
+  });
+});
+
+// check if component reference exist
+checkReference(listsNonActive, () => {
+  // -- click: non Active list item
+  listsNonActive.addEventListener('click', (event) => {
+    // call function when checklist clicked
+    clickCheck(event);
+    clickCross(event);
+  });
+});
+
+// return all items in list array as dom string
+function renderLists(lists, checked = false) {
+  let html = '';
+  lists.forEach(item => {
+    if (item.checked === checked) html += listItem(item);
+  });
+
+  return html;
+}
+
+// function / check if component reference exist
+function checkReference(dom, callback) {
+  if (dom !== null) {
+    callback()
+  }
+}
+
+// run after loading dom
+(function () {
+  // check if component reference exist
+  checkReference(listsNonActive, () => {
+    listsActive.innerHTML = renderLists(listItems);
+  });
+
+  // check if component reference exist
+  checkReference(listsNonActive, () => {
+    listsNonActive.innerHTML = renderLists(listItems, true);
+  });
+
+})();
---
+++ profile.js
@@ -0,0 +1,120 @@
+// prepare variables
+let db;
+let dbVersion = 1;
+let dbReady = false;
+
+//  wait until dom loaded 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM Content Loaded');
+
+  // prepare Html Dom References
+  const addImage = document.getElementById('add-image');
+  const previewImage = document.getElementById('preview-image');
+
+  // listen to event
+  // -- change: upload image button
+  checkReference(addImage, () => {
+    addImage.addEventListener('change', (e) => {
+      uploadImage(e, doImageTest);
+    });
+  });
+
+  // -- click: user image
+  checkReference(previewImage, () => {
+    previewImage.addEventListener('click', doImageTest);
+  });
+
+
+
+  initDB();
+});
+
+function initDB() {
+  // open connection to indexedDB Database and store to request
+  let request = indexedDB.open('myFiles', dbVersion);
+
+  // handle error request
+  request.onerror = function (e) {
+    console.error('Unable to open database.');
+  }
+
+  // handle success request
+  request.onsuccess = function (e) {
+    db = e.target.result;
+    console.log('db opened');
+  }
+
+  // send request
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result;
+    // create object store
+    db.createObjectStore('myImages', {
+      keyPath: 'id',
+      autoIncrement: true
+    });
+
+    // set db to ready
+    dbReady = true;
+  }
+}
+
+// function / upload image
+function uploadImage(e, callback) {
+  console.log('change event fired for input field');
+  // store uploaded file
+  let file = e.target.files[0];
+  console.log(file);
+
+
+  let reader = new FileReader();
+  reader.readAsBinaryString(file);
+
+  // process upload to db
+  reader.onload = function (e) {
+    // store file as bits
+    let bits = e.target.result;
+
+    // store bits to new object
+    let ob = {
+      created: new Date(),
+      data: bits
+    };
+
+    // store db transaction and add request
+    let trans = db.transaction(['myImages'], 'readwrite');
+    let addReq = trans.objectStore('myImages').add(ob);
+
+    // add response to error or success
+    addReq.onerror = function (e) {
+      console.log('error storing data');
+      console.error(e);
+    }
+
+    trans.oncomplete = function (e) {
+      console.log('data stored');
+      callback();
+    }
+
+  }
+}
+
+// test displaying images
+function doImageTest() {
+  console.log('Do image test');
+  const image = document.getElementById('image-test');
+
+  // open db transaction
+  let trans = db.transaction(['myImages'], 'readonly');
+
+  // request file
+  let req = trans.objectStore('myImages').get(1);
+
+  // handle success request
+  req.onsuccess = function (e) {
+    let record = e.target.result;
+
+    console.log('get Files success', record);
+    image.src = 'data:image/jpeg;base64,' + btoa(record.data);
+
+  }
+}
